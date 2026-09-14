3645f3ff5cd2377ad784efadf194fb594b3cacc4
Shafikuzzaman <rafsansourov26@gmail.com>
2026-09-14 14:53:23 +0600

    Update DataTypes.c

--- W3Schools_C_ Tutorial/DataTypes.c
+++ W3Schools_C_ Tutorial/DataTypes.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+    int studentID=101;
+    float score=95.34;
+    char grade='A';
+    printf("%d",studentID);
+    printf("%.2f",score);
+    printf("%c",grade);
+}
